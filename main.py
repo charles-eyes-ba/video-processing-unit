42cdad298e4c475dee5675b81fb6bf54ef6f8e44
Breno Aquino <brenoaquino7phr@gmail.com>
2021-11-29 00:11:32 -0300

    removing japan cam

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.domain.configs.cams import CAM_CROSSING, CAM_STREET, CAM_EASY_STREET, CAM_JAPAN_ROAD
+from src.domain.configs.cams import CAM_CROSSING, CAM_STREET, CAM_EASY_STREET
 from src.domain.configs.yolo_paths import YOLO_CONFIG_PATH, YOLO_WEIGHTS_PATH, YOLO_CLASSES_PATH
 
 from src.external.opencv.live_video_capture import LiveVideoCapture
@@ -9,7 +9,7 @@ import cv2
 yolo = YoloDNN(YOLO_CONFIG_PATH, YOLO_WEIGHTS_PATH, YOLO_CLASSES_PATH)
 
 cams = []
-for cam in [CAM_CROSSING, CAM_EASY_STREET, CAM_JAPAN_ROAD]:
+for cam in [CAM_CROSSING, CAM_STREET, CAM_EASY_STREET]:
     cams.append((str(cam), LiveVideoCapture(cam)))
 
 while True:
--- src/domain/configs/cams.py
+++ src/domain/configs/cams.py
@@ -1,4 +1,3 @@
 CAM_STREET = 'http://166.165.35.32/mjpg/video.mjpg'
 CAM_CROSSING = 'http://62.242.189.219:80/mjpg/video.mjpg'
-CAM_JAPAN_ROAD = 'http://183.105.80.90:5000/webcapture.jpg?command=snap&channel=1?1636932508'
 CAM_EASY_STREET = 'http://62.45.104.42:8083/mjpg/video.mjpg'
