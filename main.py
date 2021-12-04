0d89bf8c72bc3400e3c164d114877be7c23a0756
Breno Aquino <brenoaquino7phr@gmail.com>
2021-12-04 20:31:44 -0300

    removing comments

--- main.py
+++ main.py
@@ -2,31 +2,3 @@ from src.app import VideoProcessingUnit
 
 vpu = VideoProcessingUnit()
 vpu.start()
-
-# camera_1 = VideoProcessor(
-#     id="CAM_HOUSE_EXTERNAL_RIGHT",
-#     video_feed=VideoFeed(CAM_HOUSE_EXTERNAL_RIGHT),
-#     dnn=DeepNeuralNetwork(YOLO_CONFIG_PATH, YOLO_WEIGHTS_PATH, YOLO_CLASSES_PATH),
-#     websocket=websocket
-# )
-
-# camera_2 = VideoProcessor(
-#     id="CAM_HOUSE_EXTERNAL_LEFT",
-#     video_feed=VideoFeed(CAM_HOUSE_EXTERNAL_LEFT),
-#     dnn=DeepNeuralNetwork(YOLO_CONFIG_PATH, YOLO_WEIGHTS_PATH, YOLO_CLASSES_PATH),
-#     websocket=websocket
-# )
-
-# camera_3 = VideoProcessor(
-#     id="CAM_HOUSE_GARAGE",
-#     video_feed=VideoFeed(CAM_HOUSE_GARAGE),
-#     dnn=DeepNeuralNetwork(YOLO_CONFIG_PATH, YOLO_WEIGHTS_PATH, YOLO_CLASSES_PATH),
-#     websocket=websocket
-# )
-
-# camera_4 = VideoProcessor(
-#     id="CAM_HOUSE_BACKYARD",
-#     video_feed=VideoFeed(CAM_HOUSE_BACKYARD),
-#     dnn=DeepNeuralNetwork(YOLO_CONFIG_PATH, YOLO_WEIGHTS_PATH, YOLO_CLASSES_PATH),
-#     websocket=websocket
-# )
