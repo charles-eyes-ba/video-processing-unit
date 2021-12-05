85cc6afa1e9705020f18e2067925ede1a51791c2
Breno Aquino <brenoaquino7phr@gmail.com>
2021-12-05 19:51:00 -0300

    update doc

--- src/websocket/websocket_client.py
+++ src/websocket/websocket_client.py
@@ -8,19 +8,42 @@ class WebSocketClient:
     """ 
     Class that handles the websocket connection 
 
-    Parameters
+    Attributes
     ----------
-    url : str
-        The url of the websocket server
+    on_video_feeds_update : function
+        Function that is called when the all video feeds are updated
+    on_add_video_feed : function
+        Function that is called when a new video feed is added
+    on_remove_video_feed : function
+        Function that is called when a video feed is removed
+    root_namespace : RootNamespace
+        The root namespace. Can be used to send messages in websocket with root namespace 
+    detection_namespace : DetectionNamespace
+        The detection namespace. Can be used to send messages in websocket with detection namespace
+    config_namespace : ConfigNamespace
+        The config namespace. Can be used to send messages in websocket with config namespace
+    
+    Methods
+    -------
+    request_configs()
+        Request configs from server
+    send_detections(id, classes)
+        Send detections to server
     """
     def __init__(self, url):
+        """
+        Parameters
+        ----------
+        url : str
+            The url of the websocket server
+        """
         self.on_video_feeds_update = None
         self.on_add_video_feed = None
         self.on_remove_video_feed = None
         
-        self.root_namespace = self.generate_root_namespace()
-        self.detection_namespace = self.generate_detection_namespace()
-        self.config_namespace = self.generate_config_namespace()
+        self.root_namespace = self._generate_root_namespace()
+        self.detection_namespace = self._generate_detection_namespace()
+        self.config_namespace = self._generate_config_namespace()
 
         self._socketio = socketio.Client()
         self._socketio.register_namespace(self.root_namespace)
@@ -32,40 +55,61 @@ class WebSocketClient:
 
 
     # * Setups Namespaces
-    def generate_root_namespace(self):
+    def _generate_root_namespace(self):
         """ Generate the root namespace """
         return RootNamespace('/')
 
 
-    def generate_detection_namespace(self):
+    def _generate_detection_namespace(self):
         """ Generate the detection namespace """
         return DetectionNamespace('/detection')
 
 
-    def generate_config_namespace(self):
+    def _generate_config_namespace(self):
         """ Generate the config namespace """
         config_namespace = ConfigNamespace('/config')
-        config_namespace.on_request_unit_configuration = self.on_request_unit_configuration
-        config_namespace.on_add_camera = self.on_add_camera
-        config_namespace.on_remove_camera = self.on_remove_camera
+        config_namespace.on_request_unit_configuration = self._on_request_unit_configuration
+        config_namespace.on_add_camera = self._on_add_camera
+        config_namespace.on_remove_camera = self._on_remove_camera
         return config_namespace
     
 
     # * Receive Config Namespace
-    def on_request_unit_configuration(self, config):
-        """ Receive configs from server """
+    def _on_request_unit_configuration(self, config):
+        """ 
+        Receive configs from server 
+        
+        Parameters
+        ----------
+        config : dict
+            All video feeds configs
+        """
         if self.on_video_feeds_update != None:
             self.on_video_feeds_update(config['cameras'])
             
             
-    def on_add_camera(self, vide_feed):
-        """ Add a video feed to the server """
+    def _on_add_camera(self, video_feed):
+        """ 
+        Add a video feed to the server 
+        
+        Parameters
+        ----------
+        video_feed : dict
+            The video feed to add
+        """
         if self.on_add_video_feed != None:
-            self.on_add_video_feed(vide_feed)
+            self.on_add_video_feed(video_feed)
             
             
-    def on_remove_camera(self, video_feed_id):
-        """ Remove a video feed from the server """
+    def _on_remove_camera(self, video_feed_id):
+        """ 
+        Remove a video feed from the server 
+        
+        Parameters
+        ----------
+        video_feed_id : str
+            The id of the video feed to remove
+        """
         if self.on_remove_video_feed != None:
             self.on_remove_video_feed(video_feed_id)
         
@@ -77,6 +121,14 @@ class WebSocketClient:
 
 
     def send_detections(self, id, classes):
-        """ Send detections to server """
-        print(f'Sending detections {id}')
+        """ 
+        Send detections to server 
+        
+        Parameters
+        ----------
+        id : str
+            The id of the video feed
+        classes : list
+            The classes name detected in the video feed
+        """
         self.detection_namespace.emit('detect', { 'id': id, 'detections': classes })
