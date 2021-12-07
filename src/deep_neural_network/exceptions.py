2403767303b995972c47ab902a63b561fdb8195f
Breno Aquino <brenoaquino7phr@gmail.com>
2021-12-07 20:40:55 -0300

    update doc

--- src/deep_neural_network/exceptions.py
+++ src/deep_neural_network/exceptions.py
@@ -1,3 +1,10 @@
 class InvalidDeepNeuralNetworkFilesException(Exception):
+    """ Exception for invalid Deep Neural Network files """
     def __init__(self, message):
+        """
+        Parameters
+        ----------
+        message : str
+            The message with error details
+        """
         self.message = message
