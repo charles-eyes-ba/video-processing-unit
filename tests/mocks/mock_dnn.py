0c565cda65c70baadf5246b19aba0ae009c5605b
Breno Aquino <brenoaquino7phr@gmail.com>
2021-12-11 13:52:40 -0300

    create frame collector mock

--- tests/mocks/mock_dnn.py
+++ tests/mocks/mock_dnn.py
@@ -1,6 +1,7 @@
 from src.external.deep_neural_network import DeepNeuralNetwork
 
 class MockDeepNeuralNetwork(DeepNeuralNetwork):
+    
     def __init__(self, config_path, weights_path, classes_path, __init__=None, predict=None):
         self._config_path = config_path 
         self._weights_path = weights_path 
---
+++ tests/mocks/mock_frame_collector.py
@@ -0,0 +1,37 @@
+from src.domain.frame_collector import FrameCollector
+
+class MockFrameCollector(FrameCollector):
+    
+    def __init__(self, video_capture, __init__=None, setup_callbacks=None, start=None, stop=None, pop_lastest_frame=None, release=None):
+        self._video_capture = video_capture
+        self._init = __init__
+        self._setup_callbacks = setup_callbacks
+        self._start = start
+        self._stop = stop
+        self._pop_lastest_frame = pop_lastest_frame
+        self._release = release
+        
+        
+    def setup_callbacks(self, on_error=None):
+        if self._setup_callbacks is not None:
+            self._setup_callbacks(on_error)
+        
+      
+    def start(self):
+        if self._start is not None:
+            self._start()
+    
+    
+    def stop(self):
+        if self._stop is not None:
+            self._stop()
+    
+    
+    def pop_lastest_frame(self):
+        if self._pop_lastest_frame is not None:
+            return self._pop_lastest_frame()
+    
+    
+    def release(self):
+        if self._release is not None:
+            self._release()
