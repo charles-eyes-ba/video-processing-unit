995fa9fc31589d451471eb96cb3826d7849fda68
Breno Aquino <brenoaquino7phr@gmail.com>
2021-12-11 11:34:51 -0300

    improve video capture exceptions

--- src/external/video_capture/video_capture_opencv.py
+++ src/external/video_capture/video_capture_opencv.py
@@ -1,11 +1,19 @@
 from .interface import VideoCapture
+from .exceptions import VideoCaptureCouldNotConnect, VideoCaptureConnectionLost
 
 import cv2
 
 class VideoCaptureOpenCV(VideoCapture):
     def __init__(self, path):
-        self.cap = cv2.VideoCapture(path)
+        self.__path = path
+        self._cap = cv2.VideoCapture(self.__path)
+        if self._cap is None or not self._cap.isOpened():
+            raise VideoCaptureCouldNotConnect(f'Could not connect to video source: {self.__path}')
+        self._cap.setExceptionMode(True)
         
     def read(self):
-        _, frame = self.cap.read()
+        try:
+            _, frame = self._cap.read()
+        except:
+            raise VideoCaptureConnectionLost(f'Connection to video source lost: {self.__path}')
         return frame
