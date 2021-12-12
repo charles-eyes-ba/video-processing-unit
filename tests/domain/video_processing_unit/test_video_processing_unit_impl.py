da9dfa62fba04dd77be3af762a6f3cc8a950c47d
Breno Aquino <brenoaquino7phr@gmail.com>
2021-12-12 01:20:57 -0300

    create tests case

---
+++ tests/domain/video_processing_unit/test_video_processing_unit_impl.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+
+class VideoProcessingUnitTests(TestCase):
+    
+    def test_add_video_feed(self):
+        self.assertFalse(True)
+    
+    
+    def test_add_video_same_id_in_list(self):
+        self.assertFalse(True)
+    
+    
+    def test_add_video_feed_without_id(self):
+        self.assertFalse(True)
+        
+        
+    def test_add_video_feed_without_url(self):
+        self.assertFalse(True)
+        
+        
+    def test_add_video_feed_with_error_in_create_detector(self):
+        self.assertFalse(True)
+        
+    
+    def test_remove_video_feed(self):
+        self.assertFalse(True)
+    
+    
+    def test_remove_video_feed_without_id_in_list(self):
+        self.assertFalse(True)
+    
+    
+    def test_update_video_feed_list_with_empty_list(self):
+        self.assertFalse(True)
+        
+        
+    def test_update_video_feed_list_with_non_empty_list(self):
+        self.assertFalse(True)
+        
+        
+    def test_detection_callback(self):
+        self.assertFalse(True)
+        
+        
+    def test_error_callback(self):
+        self.assertFalse(True)
+        
+        
+    def test_start(self):
+        self.assertFalse(True)
