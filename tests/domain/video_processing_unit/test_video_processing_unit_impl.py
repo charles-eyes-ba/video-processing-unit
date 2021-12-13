46a6004559b3e48dec340add649bb969c18cb3bb
Breno Aquino <brenoaquino7phr@gmail.com>
2021-12-13 12:50:24 -0300

    add unit tests again

--- tests/domain/video_processing_unit/test_video_processing_unit_impl.py
+++ tests/domain/video_processing_unit/test_video_processing_unit_impl.py
@@ -41,178 +41,178 @@ class VideoProcessingUnitTests(TestCase):
         self.assertIsNotNone(vpu._detectors[0]._on_error)
     
     
-    # def test_vpu_add_video_same_id_in_list(self):
-    #     # Given
-    #     websocket = MockWebSocket()
-    #     vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
-    #     video_feed_to_start = { 'id': '1', 'feed_url': 'https://google.com' }
-    #     vpu._add_video_feed(video_feed_to_start)
-    #     video_feed_to_add = { 'id': '1', 'feed_url': 'https://charles.com' }
+    def test_vpu_add_video_same_id_in_list(self):
+        # Given
+        websocket = MockWebSocket()
+        vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
+        video_feed_to_start = { 'id': '1', 'feed_url': 'https://google.com' }
+        vpu._add_video_feed(video_feed_to_start)
+        video_feed_to_add = { 'id': '1', 'feed_url': 'https://charles.com' }
         
-    #     # When
-    #     vpu._add_video_feed(video_feed_to_add)
+        # When
+        vpu._add_video_feed(video_feed_to_add)
         
-    #     # Then
-    #     self.assertEqual(len(vpu._detectors), 1)
-    #     self.assertEqual(vpu._detectors[0].id, '1')
-    #     self.assertEqual(vpu._detectors[0]._frame_collector._video_capture.url, 'https://google.com')
+        # Then
+        self.assertEqual(len(vpu._detectors), 1)
+        self.assertEqual(vpu._detectors[0].id, '1')
+        self.assertEqual(vpu._detectors[0]._frame_collector._video_capture.url, 'https://google.com')
     
     
-    # def test_vpu_add_video_feed_without_id(self):
-    #     # Given
-    #     websocket = MockWebSocket()
-    #     vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
-    #     video_feed_to_add = { 'feed_url': 'https://google.com' }
+    def test_vpu_add_video_feed_without_id(self):
+        # Given
+        websocket = MockWebSocket()
+        vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
+        video_feed_to_add = { 'feed_url': 'https://google.com' }
         
-    #     # When
-    #     vpu._add_video_feed(video_feed_to_add)
+        # When
+        vpu._add_video_feed(video_feed_to_add)
         
-    #     # Then
-    #     self.assertEqual(len(vpu._detectors), 0)
-    #     self.assertTrue(websocket.sent_error)
-    #     self.assertEqual(websocket.sent_error_params[0], 'None')
-    #     self.assertIsInstance(websocket.sent_error_params[1], CameraParamsNotFoundException)
-    #     self.assertEqual(websocket.sent_error_params[1].message, 'Not found id')
-        
-        
-    # def test_vpu_add_video_feed_without_url(self):
-    #     # Given
-    #     websocket = MockWebSocket()
-    #     vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
-    #     video_feed_to_add = { 'id': '1' }
-        
-    #     # When
-    #     vpu._add_video_feed(video_feed_to_add)
-        
-    #     # Then
-    #     self.assertEqual(len(vpu._detectors), 0)
-    #     self.assertTrue(websocket.sent_error)
-    #     self.assertEqual(websocket.sent_error_params[0], 'None')
-    #     self.assertIsInstance(websocket.sent_error_params[1], CameraParamsNotFoundException)
-    #     self.assertEqual(websocket.sent_error_params[1].message, 'Not found feed_url')
+        # Then
+        self.assertEqual(len(vpu._detectors), 0)
+        self.assertTrue(websocket.sent_error)
+        self.assertEqual(websocket.sent_error_params[0], 'None')
+        self.assertIsInstance(websocket.sent_error_params[1], CameraParamsNotFoundException)
+        self.assertEqual(websocket.sent_error_params[1].message, 'Not found id')
         
         
-    # def test_vpu_add_video_feed_with_error_in_create_detector(self):
-    #     # Given
-    #     def throw_exception_creation(id, url, config_path,  weights_path,  classes_path):
-    #         raise InvalidDeepNeuralNetworkFilesException('Test Exception')
-    #     websocket = MockWebSocket()
-    #     vpu = VideoProcessingUnitImpl(websocket, throw_exception_creation)
-    #     video_feed_to_add = { 'id': '1', 'feed_url': 'https://google.com' }
+    def test_vpu_add_video_feed_without_url(self):
+        # Given
+        websocket = MockWebSocket()
+        vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
+        video_feed_to_add = { 'id': '1' }
         
-    #     # When
-    #     vpu._add_video_feed(video_feed_to_add)
+        # When
+        vpu._add_video_feed(video_feed_to_add)
         
-    #     # Then
-    #     self.assertEqual(len(vpu._detectors), 0)
-    #     self.assertTrue(websocket.sent_error)
-    #     self.assertEqual(websocket.sent_error_params[0], '1')
-    #     self.assertIsInstance(websocket.sent_error_params[1], InvalidDeepNeuralNetworkFilesException)
-    #     self.assertEqual(websocket.sent_error_params[1].message, 'Test Exception')
+        # Then
+        self.assertEqual(len(vpu._detectors), 0)
+        self.assertTrue(websocket.sent_error)
+        self.assertEqual(websocket.sent_error_params[0], 'None')
+        self.assertIsInstance(websocket.sent_error_params[1], CameraParamsNotFoundException)
+        self.assertEqual(websocket.sent_error_params[1].message, 'Not found feed_url')
+        
+        
+    def test_vpu_add_video_feed_with_error_in_create_detector(self):
+        # Given
+        def throw_exception_creation(id, url, config_path,  weights_path,  classes_path):
+            raise InvalidDeepNeuralNetworkFilesException('Test Exception')
+        websocket = MockWebSocket()
+        vpu = VideoProcessingUnitImpl(websocket, throw_exception_creation)
+        video_feed_to_add = { 'id': '1', 'feed_url': 'https://google.com' }
+        
+        # When
+        vpu._add_video_feed(video_feed_to_add)
+        
+        # Then
+        self.assertEqual(len(vpu._detectors), 0)
+        self.assertTrue(websocket.sent_error)
+        self.assertEqual(websocket.sent_error_params[0], '1')
+        self.assertIsInstance(websocket.sent_error_params[1], InvalidDeepNeuralNetworkFilesException)
+        self.assertEqual(websocket.sent_error_params[1].message, 'Test Exception')
         
     
-    # def test_vpu_remove_video_feed(self):
-    #     # Given
-    #     websocket = MockWebSocket()
-    #     vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
-    #     video_feed = { 'id': '1', 'feed_url': 'https://google.com' }
-    #     vpu._add_video_feed(video_feed)
+    def test_vpu_remove_video_feed(self):
+        # Given
+        websocket = MockWebSocket()
+        vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
+        video_feed = { 'id': '1', 'feed_url': 'https://google.com' }
+        vpu._add_video_feed(video_feed)
         
-    #     # When
-    #     vpu._remove_video_feed('1')
+        # When
+        vpu._remove_video_feed('1')
         
-    #     # Then
-    #     self.assertEqual(len(vpu._detectors), 0)
+        # Then
+        self.assertEqual(len(vpu._detectors), 0)
     
     
-    # def test_vpu_remove_video_feed_without_id_in_list(self):
-    #     # Given
-    #     websocket = MockWebSocket()
-    #     vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
-    #     video_feed = { 'id': '1', 'feed_url': 'https://google.com' }
-    #     vpu._add_video_feed(video_feed)
+    def test_vpu_remove_video_feed_without_id_in_list(self):
+        # Given
+        websocket = MockWebSocket()
+        vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
+        video_feed = { 'id': '1', 'feed_url': 'https://google.com' }
+        vpu._add_video_feed(video_feed)
         
-    #     # When
-    #     vpu._remove_video_feed('2')
+        # When
+        vpu._remove_video_feed('2')
         
-    #     # Then
-    #     self.assertEqual(len(vpu._detectors), 1)
+        # Then
+        self.assertEqual(len(vpu._detectors), 1)
     
     
-    # def test_vpu_update_video_feed_list_with_empty_list(self):
-    #     # Given
-    #     websocket = MockWebSocket()
-    #     vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
-    #     video_feeds = [
-    #         { 'id': '1', 'feed_url': 'https://google.com' },
-    #         { 'id': '2', 'feed_url': 'https://charles.com' },
-    #     ]
+    def test_vpu_update_video_feed_list_with_empty_list(self):
+        # Given
+        websocket = MockWebSocket()
+        vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
+        video_feeds = [
+            { 'id': '1', 'feed_url': 'https://google.com' },
+            { 'id': '2', 'feed_url': 'https://charles.com' },
+        ]
         
-    #     # When
-    #     vpu._update_video_feed_list(video_feeds)
+        # When
+        vpu._update_video_feed_list(video_feeds)
         
-    #     # Then
-    #     self.assertEqual(len(vpu._detectors), 2)
-    #     self.assertEqual(vpu._detectors[0].id, '1')
-    #     self.assertEqual(vpu._detectors[0]._frame_collector._video_capture.url, 'https://google.com')
-    #     self.assertIsNotNone(vpu._detectors[0]._on_object_detection)
-    #     self.assertIsNotNone(vpu._detectors[0]._on_error)
-    #     self.assertEqual(vpu._detectors[1].id, '2')
-    #     self.assertEqual(vpu._detectors[1]._frame_collector._video_capture.url, 'https://charles.com')
-    #     self.assertIsNotNone(vpu._detectors[1]._on_object_detection)
-    #     self.assertIsNotNone(vpu._detectors[1]._on_error)
-        
-        
-    # def test_vpu_update_video_feed_list_with_non_empty_list(self):
-    #     # Given
-    #     websocket = MockWebSocket()
-    #     vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
-    #     video_feed = { 'id': '1', 'feed_url': 'https://google.com' }
-    #     vpu._add_video_feed(video_feed)
-    #     video_feeds = [
-    #         { 'id': '1', 'feed_url': 'https://google.com' },
-    #         { 'id': '2', 'feed_url': 'https://charles.com' },
-    #     ]
-        
-    #     # When
-    #     vpu._update_video_feed_list(video_feeds)
-        
-    #     # Then
-    #     self.assertEqual(len(vpu._detectors), 2)
-    #     self.assertEqual(vpu._detectors[0].id, '1')
-    #     self.assertEqual(vpu._detectors[0]._frame_collector._video_capture.url, 'https://google.com')
-    #     self.assertIsNotNone(vpu._detectors[0]._on_object_detection)
-    #     self.assertIsNotNone(vpu._detectors[0]._on_error)
-    #     self.assertEqual(vpu._detectors[1].id, '2')
-    #     self.assertEqual(vpu._detectors[1]._frame_collector._video_capture.url, 'https://charles.com')
-    #     self.assertIsNotNone(vpu._detectors[1]._on_object_detection)
-    #     self.assertIsNotNone(vpu._detectors[1]._on_error)
+        # Then
+        self.assertEqual(len(vpu._detectors), 2)
+        self.assertEqual(vpu._detectors[0].id, '1')
+        self.assertEqual(vpu._detectors[0]._frame_collector._video_capture.url, 'https://google.com')
+        self.assertIsNotNone(vpu._detectors[0]._on_object_detection)
+        self.assertIsNotNone(vpu._detectors[0]._on_error)
+        self.assertEqual(vpu._detectors[1].id, '2')
+        self.assertEqual(vpu._detectors[1]._frame_collector._video_capture.url, 'https://charles.com')
+        self.assertIsNotNone(vpu._detectors[1]._on_object_detection)
+        self.assertIsNotNone(vpu._detectors[1]._on_error)
         
         
-    # def test_vpu_detection_callback(self):
-    #     # Given
-    #     websocket = MockWebSocket()
-    #     vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
+    def test_vpu_update_video_feed_list_with_non_empty_list(self):
+        # Given
+        websocket = MockWebSocket()
+        vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
+        video_feed = { 'id': '1', 'feed_url': 'https://google.com' }
+        vpu._add_video_feed(video_feed)
+        video_feeds = [
+            { 'id': '1', 'feed_url': 'https://google.com' },
+            { 'id': '2', 'feed_url': 'https://charles.com' },
+        ]
         
-    #     # When
-    #     vpu._on_detection_callback('1', ['car'])
+        # When
+        vpu._update_video_feed_list(video_feeds)
         
-    #     # Then
-    #     self.assertTrue(websocket.sent_detections, '1')
-    #     self.assertEqual(websocket.sent_detections_params[0], '1')
-    #     self.assertEqual(websocket.sent_detections_params[1], ['car'])
+        # Then
+        self.assertEqual(len(vpu._detectors), 2)
+        self.assertEqual(vpu._detectors[0].id, '1')
+        self.assertEqual(vpu._detectors[0]._frame_collector._video_capture.url, 'https://google.com')
+        self.assertIsNotNone(vpu._detectors[0]._on_object_detection)
+        self.assertIsNotNone(vpu._detectors[0]._on_error)
+        self.assertEqual(vpu._detectors[1].id, '2')
+        self.assertEqual(vpu._detectors[1]._frame_collector._video_capture.url, 'https://charles.com')
+        self.assertIsNotNone(vpu._detectors[1]._on_object_detection)
+        self.assertIsNotNone(vpu._detectors[1]._on_error)
         
         
-    # def test_vpu_error_callback(self):
-    #     # Given
-    #     websocket = MockWebSocket()
-    #     vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
+    def test_vpu_detection_callback(self):
+        # Given
+        websocket = MockWebSocket()
+        vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
         
-    #     # When
-    #     vpu._on_error_callback('1', CameraParamsNotFoundException('Test Exception'))
+        # When
+        vpu._on_detection_callback('1', ['car'])
         
-    #     # Then
-    #     self.assertTrue(websocket.sent_error)
-    #     self.assertEqual(websocket.sent_error_params[0], '1')
-    #     self.assertIsInstance(websocket.sent_error_params[1], CameraParamsNotFoundException)
-    #     self.assertEqual(websocket.sent_error_params[1].message, 'Test Exception')
+        # Then
+        self.assertTrue(websocket.sent_detections, '1')
+        self.assertEqual(websocket.sent_detections_params[0], '1')
+        self.assertEqual(websocket.sent_detections_params[1], ['car'])
+        
+        
+    def test_vpu_error_callback(self):
+        # Given
+        websocket = MockWebSocket()
+        vpu = VideoProcessingUnitImpl(websocket, self.generate_mock_detector)
+        
+        # When
+        vpu._on_error_callback('1', CameraParamsNotFoundException('Test Exception'))
+        
+        # Then
+        self.assertTrue(websocket.sent_error)
+        self.assertEqual(websocket.sent_error_params[0], '1')
+        self.assertIsInstance(websocket.sent_error_params[1], CameraParamsNotFoundException)
+        self.assertEqual(websocket.sent_error_params[1].message, 'Test Exception')
