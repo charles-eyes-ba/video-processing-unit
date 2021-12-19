91cb29b9e518c6fa07659d1d8bbb41637eb2e93c
Breno Aquino <brenoaquino7phr@gmail.com>
2021-12-19 13:55:47 -0300

    merging namespaces

--- src/external/websocket/namespaces/detection_namespace.py
+++
@@ -1,9 +0,0 @@
-from socketio import ClientNamespace
-
-class DetectionNamespace(ClientNamespace):
-    """ Class that handles the /detection namespace """
-    DETECT = 'detect'
-    ERROR = 'error'
-    
-    def __init__(self):
-        super().__init__(namespace='/detection')
